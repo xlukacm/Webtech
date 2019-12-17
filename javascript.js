0964e0d55af8fbc2740e8490bd91e35020e60828
Marius <mario.luk4@gmail.com>
2019-12-17 11:28:53 +0100

    Animacia krizovatky pomocou GSPA

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// //import { gsap } from "gsap";
+// //
+// // let svg = document.getElementById("#svg2");
+// //   let animation = gsap.timeline();
+// //
+// //     animation
+// //         .set(svg, {duration:2, x:0})
+// //         .to(svg,{duration:2,x:100})
+// //         .to(svg, {duration:2,x:200})
+// //         .to(svg, {duration:2,x:300})
+// //         .to(svg, {duration:2,x:400})
+// //         .to(svg, {duration:1,y:"-=160"})
+// //         .to(svg, {duration:1,y:"-=160"});
