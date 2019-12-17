15e3452559d59605f3daa3fee8b04b9a0f46eacc
Marius <mario.luk4@gmail.com>
2019-12-17 16:48:42 +0100

    zachadzajuce auto, rozbeh po kliknuti

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
- let svg = document.querySelector(".radsej_neotvaraj");
+ let svg = document.querySelector("#svg2");
 // var linijaDevelopment = document.querySelector(".linija-development");
 
-let animation = gsap.timeline();
-animation .set(svg, {duration: 0, ease: "none", x:0})
-    .to(svg, 1,{duration: 0, ease: "none",x:100})
-    .to(svg, 1,{duration: 0, ease: "none",x:200})
-    .to(svg, 1,{duration: 0, ease: "none",x:300, rotation:"-45_short"})
-    .to(svg, 1,{ease: "none", x:360, rotation:"-70_short"})
-    .to(svg, 1,{ease: "none", y:"-=160", rotation:"-90_short"})
-    .to(svg, 1,{ease: "none", y:"-=190"})
-    .to(svg, 1,{ease: "none", y:"-=220"});
+function move(){
+    let animation = gsap.timeline();
+    animation .set(svg, {ease: "none",     x:0})
+        .to(svg, 1,{ease: "none", x:100})
+        .to(svg, 1,{ease: "none", x:200})
+        .to(svg, 1,{ease: "none", x:250, y:"-=50",  rotation:"-40_short"})
+        .to(svg, 1,{ease: "none", x:330, y:"-100",  rotation:"-80_short"})
+        .to(svg, 1,{ease: "none",                      y:"-=160", rotation:"-90_short"})
+        .to(svg, 1,{ease: "none",                      y:"-=190"})
+        .to(svg, 1,{ease: "none",                      y:"-=220"});
+}
+
