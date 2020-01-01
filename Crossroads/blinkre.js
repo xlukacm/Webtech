59be7fa72465bb74f91b60e37b58fd023221172e
Martin Michale <Nanovisions123>
2020-01-01 15:49:45 +0100

    Dokoncenie blinkrov

--- Crossroads/blinkre.js
+++ Crossroads/blinkre.js
@@ -1,24 +1,52 @@
 function setBlinkre1(){
-    setInterval(blackBlinker1, 1000);
-    setInterval(greenBlinker1, 1000);
+    setInterval(yellowBlinker, 1000);
+    setInterval(greenBlinker, 1000);
+}
+function setBlinkre2(){
+    setInterval(yellowBlinker, 1000);
+    setInterval(greenBlinker, 1000);
+}
+function setBlinkre7(){
+    setInterval(greenBlinker, 1000);
+}
+function setBlinkre8(){
+    setInterval(yellowBlinker, 1000);
+    setInterval(greenBlinker, 1000);
+}
+function setBlinkre9(){
+    setInterval(yellowBlinker, 1000);
+    setInterval(greenBlinker, 1000);
+}
+function setBlinkre10(){
+    setInterval(greenBlinker, 1000);
+}
+function setBlinkre13(){
+    setInterval(greenBlinker, 1000);
+}
+function setBlinkre14(){
+    setInterval(yellowBlinker, 1000);
+}
+function setBlinkre15(){
+    setInterval(yellowBlinker, 1000);
+    setInterval(greenBlinker, 1000);
 }
 
-let blackBlinker1On = 0;
-function blackBlinker1(){
-    if (blackBlinker1On===0){
+let yellowBlinker1On = 0;
+function yellowBlinker(){
+    if (yellowBlinker1On===0){
         document.getElementById("yellowCar").style.display = "none";
         document.getElementById("yellowCarB").style.display = "inherit";
-        blackBlinker1On = 1;
+        yellowBlinker1On = 1;
     }
-    else if (blackBlinker1On===1){
+    else if (yellowBlinker1On===1){
         document.getElementById("yellowCar").style.display = "inherit";
         document.getElementById("yellowCarB").style.display = "none";
-        blackBlinker1On = 0;
+        yellowBlinker1On = 0;
     }
 }
 
 let greenBlinker1On = 0;
-function greenBlinker1(){
+function greenBlinker(){
     if (greenBlinker1On===0){
         document.getElementById("greenCar").style.display = "none";
         document.getElementById("greenCarB").style.display = "inherit";
