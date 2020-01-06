e0dde0b71aa9cc9685d384a8af847d6cfe484e0d
Paťo <pato2582@gmail.com>
2020-01-06 12:25:46 +0100

    Grafické úpravy

--- JS/meninyJS.js
+++ JS/meninyJS.js
@@ -5,7 +5,6 @@ function loadDoc() {
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
             xmlDocNameday = this.responseXML;
-            meninyDnes();
             clearDisplay();
         }
     };
